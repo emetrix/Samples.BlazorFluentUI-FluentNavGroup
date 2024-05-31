14eed439e656b42689502d87a9955d28abc3e7b1
Tobias Schaller <tschaller@emetrix.de>
2024-05-31 13:03:11 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using BlazorApp1.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace BlazorApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            builder.Services.AddFluentUIComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
